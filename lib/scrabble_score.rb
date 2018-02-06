7465b6692fa333eedb130447a90a1444f1fdfa52
kimberly huynh and Ron Craig <kimberly@>
2018-02-06 11:19:18 -0800

    add first spec, score for a letter

---
+++ lib/scrabble_score.rb
@@ -0,0 +1,16 @@
+class Scrabble
+  def initialize(word)
+    @word = word
+  end
+
+
+
+  def score
+    # score = Hash.new()
+    # score.store("a", 1)
+    if @word == "a"
+      1
+    end
+
+  end
+end
---
+++ spec/scrabble_spec.rb
@@ -0,0 +1,14 @@
+require('rspec')
+require('pry')
+require('scrabble_score')
+
+describe('#scrabble_score') do
+  it ("returns a scrabble score for a letter") do
+    word = Scrabble.new("a")
+    expect(word.score()).to(eq(1))
+  end
+  # it ("returns a scrabble score for 3 letters") do
+  #   word = Scrabble.new("dog")
+  #   expect(word.score()).to(eq(5))
+  # end
+end
