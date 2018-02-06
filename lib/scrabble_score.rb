148350be3c9f1ef3ad9dc85fd7fe6485a66b8786
kimberly huynh and Ron Craig <ron@>
2018-02-06 14:42:02 -0800

    add second spec to add words

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -3,14 +3,44 @@ class Scrabble
     @word = word
   end
 
-
-
   def score
-    # score = Hash.new()
-    # score.store("a", 1)
-    if @word == "a"
-      1
+      score = Hash.new()
+      score.store("a", 1)
+      score.store("e", 1)
+      score.store("i", 1)
+      score.store("o", 1)
+      score.store("u", 1)
+      score.store("l", 1)
+      score.store("n", 1)
+      score.store("r", 1)
+      score.store("s", 1)
+      score.store("t", 1)
+      score.store("d", 2)
+      score.store("g", 2)
+      score.store("b", 3)
+      score.store("c", 3)
+      score.store("m", 3)
+      score.store("p", 3)
+      score.store("f", 4)
+      score.store("h", 4)
+      score.store("v", 4)
+      score.store("w", 4)
+      score.store("y", 4)
+      score.store("k", 5)
+      score.store("j", 8)
+      score.store("x", 8)
+      score.store("q", 10)
+      score.store("z", 10)
+
+    words = @word.split('')
+    total = 0
+
+    words.each do |array_element|
+      point = score.fetch(array_element)
+      total += point
     end
+    total
 
   end
+
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -7,8 +7,8 @@ describe('#scrabble_score') do
     word = Scrabble.new("a")
     expect(word.score()).to(eq(1))
   end
-  # it ("returns a scrabble score for 3 letters") do
-  #   word = Scrabble.new("dog")
-  #   expect(word.score()).to(eq(5))
-  # end
+  it ("returns a scrabble score for 3 letters") do
+    word = Scrabble.new("dog")
+    expect(word.score()).to(eq(5))
+  end
 end
