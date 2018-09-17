b902ffab5083b222a1e6812d22280e2b252387e6
Ron Craig <ron.craig@comcast.net>
2018-09-16 23:54:04 -0700

    add four specs, 4 out of 4 specs are passing

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -2,13 +2,24 @@ require('rspec')
 require('pry')
 require('scrabble_score')
 
-describe('#scrabble_score') do
-  it ("returns a scrabble score for a letter") do
-    word = Scrabble.new("a")
-    expect(word.score()).to(eq(1))
+describe('scrabble') do
+  it('returns a scrabble score for a letter') do
+    word = Word.new('a')
+    expect(word.scrabble).to(eq(1))
   end
-  it ("returns a scrabble score for 3 letters") do
-    word = Scrabble.new("dog")
-    expect(word.score()).to(eq(5))
+
+  it('returns a scrabble score for a 2 letter word') do
+    word = Word.new('by')
+    expect(word.scrabble).to(eq(7))
+  end
+
+  it('returns a scrabble score for a 3 letter word') do
+    word = Word.new('dog')
+    expect(word.scrabble).to(eq(5))
+  end
+
+  it('returns a scrabble score for a long 6 letter word') do
+    word = Word.new('zagged')
+    expect(word.scrabble).to(eq(18))
   end
 end
